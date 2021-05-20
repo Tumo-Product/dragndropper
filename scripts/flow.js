3178a8e0b5041fbc319a6af2458b9632b070ddde
HaykAvagyan1 <hayk.avagyan@tumo.org>
2021-05-20 18:17:40 +0400

    Testing drag and drop with phaser
    Probably going to switch to canvas.

--- scripts/flow.js
+++ scripts/flow.js
@@ -1,90 +1,51 @@
-let app = new PIXI.Application({width: 800, height: 600});
-
-//Add the canvas that Pixi automatically created for you to the HTML document
-document.body.appendChild(app.view);
-
-// create the root of the scene graph
-let stage = new PIXI.Container();
-
-// create a texture from an image path
-let texture = PIXI.Texture.from('images/circle.png');
-
-for (let i = 0; i < 10; i++) {
-    createBunny(Math.floor(Math.random() * 800), Math.floor(Math.random() * 600));
-}
-
-function createBunny(x, y) {
-    // create our little bunny friend..
-    let bunny = new PIXI.Sprite(texture);
-    bunny.width = 92;
-    bunny.height = 51;
-
-    // enable the bunny to be interactive... this will allow it to respond to mouse and touch events
-    bunny.interactive = true;
-
-    // this button mode will mean the hand cursor appears when you roll over the bunny with your mouse
-    bunny.buttonMode = true;
-
-    // center the bunny's anchor point
-    bunny.anchor.set(0.5);
-
-    // make it a bit bigger, so it's easier to grab
-    bunny.scale.set(3);
-
-    // setup events
-    bunny
-        // events for drag start
-        .on('mousedown', onDragStart)
-        .on('touchstart', onDragStart)
-        // events for drag end
-        .on('mouseup', onDragEnd)
-        .on('mouseupoutside', onDragEnd)
-        .on('touchend', onDragEnd)
-        .on('touchendoutside', onDragEnd)
-        // events for drag move
-        .on('mousemove', onDragMove)
-        .on('touchmove', onDragMove);
-
-    // move the sprite to its designated position
-    bunny.position.x = x;
-    bunny.position.y = y;
-
-    // add it to the stage
-    stage.addChild(bunny);
-}
-
-requestAnimationFrame(animate);
-
-function animate() {
-
-    requestAnimationFrame(animate);
-
-    // render the stage
-    app.render(stage);
-}
-
-function onDragStart(event) {
-    // store a reference to the data
-    // the reason for this is because of multitouch
-    // we want to track the movement of this particular touch
-    this.data = event.data;
-    this.alpha = 0.5;
-    this.dragging = true;
-}
-
-function onDragEnd() {
-    this.alpha = 1;
-
-    this.dragging = false;
-
-    // set the interaction data to null
-    this.data = null;
-}
-
-function onDragMove() {
-    if (this.dragging) {
-        let newPosition = this.data.getLocalPosition(this.parent);
-        this.position.x = newPosition.x;
-        this.position.y = newPosition.y;
+let config = {
+    type: Phaser.WEBGL,
+    backgroundColor: "#ffffff",
+    width: window.innerWidth,
+    height: window.innerHeight,
+    parent: 'phaser-example',
+    scene: {
+        preload: preload,
+        create: create
     }
+};
+
+let game = new Phaser.Game(config);
+
+function preload() {
+    this.load.image('mario', '../images/circle.png');
+    this.load.image('bg', '../images/background.jpg')
+    this.scale.scaleMode = Phaser.Scale.ScaleModes.RESIZE;
+}
+
+function create() {
+    let background = this.add.sprite(window.innerWidth / 2, window.innerHeight / 2, 'bg');
+    let image = this.add.sprite(200, 300, 'mario').setInteractive();
+    image.setScale(.05);
+    background.setScale(.5);
+
+    image.on('pointerover', function () {
+        this.setTint(0x00ff00);
+    });
+
+    image.on('pointerout', function () {
+        this.clearTint();
+    });
+
+    this.input.setDraggable(image);
+
+    this.input.on('dragstart', function (pointer, gameObject) {
+        gameObject.setTint(0xff0000);
+    });
+
+    this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
+        gameObject.x = dragX;
+        gameObject.y = dragY;
+    });
+
+    this.input.on('dragend', function (pointer, gameObject) {
+
+        gameObject.clearTint();
+
+    });
 }
