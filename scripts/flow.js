cae33c49f8c7fdd8ec10b59aa22a703e8eeb29d6
HaykAvagyan1 <hayk.avagyan@tumo.org>
2021-05-25 17:51:12 +0400

    testing phaser again

--- scripts/flow.js
+++ scripts/flow.js
@@ -1,111 +1,59 @@
-let canvas = document.getElementById('canvas');
+const width = 1920;
+const height = 1080;
 
-let BB = canvas.getBoundingClientRect();
-let offsetX = BB.left, offsetY = BB.top;
-let dragOk = false;
-let startX, startY;
+let config = {
+    type: Phaser.AUTO,
+    backgroundColor: '#2dab2d',
+    parent: 'phaser-example',
+    scale: {
+        mode: Phaser.Scale.FIT,
+        _parent: 'phaser-example',
+        width: width,
+        height: height
+    },
+    scene: {
+        preload: preload,
+        create: create
+    },
+    transparent : true
+};
 
-let areaSize = 90;
+let game = new Phaser.Game(config);
 
-let circles = [];
-
-const mouseDown = (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-
-    // Mouse position
-    let mx = parseInt(e.clientX - offsetX);
-    let my = parseInt(e.clientY - offsetY);
-
-    dragOk = false;
-    for (let circle of circles) {
-        if (!circle.locked) {
-            if (intersects(mx, my, circle)) {
-                dragOk = true;
-                circle.dragging = true;
-            }
-        }
-    }
-
-    startX = mx;
-    startY = my;
-}
-
-const intersects = (x, y, circle) => {
-    if (x > circle.x && x < circle.x + circle.width && y > circle.y && y < circle.y + circle.height) return true;
-    else return false;
-}
-
-const mouseUp = (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-
-    dragOk = false;
-
-    let mx = parseInt(e.clientX - offsetX);
-    let my = parseInt(e.clientY - offsetY);
-
-    for (let circle of circles) {
-        if (circle.dragging) {
-            circle.dragging = false;
-
-            if (!circle.locked) {
-                if (intersects(mx, my, { x: circle.stick.x, y: circle.stick.y, width: areaSize, height: areaSize })) {
-                    circle.x = circle.stick.x;
-                    circle.y = circle.stick.y;
-                    gfx.redrawImage(circle.img, circle.stick.x, circle.stick.y, circle.width, circle.height, circles);
-                    circle.locked = true;
-                } else {
-                    gfx.redrawImage(circle.img, circle.x, circle.y, circle.width, circle.height, circles);
-                }
-            }
-        }
-    }
-}
-
-const mouseMove = (e) => {
-    if (dragOk) {
-        // Mouse position
-        let mx = parseInt(e.clientX - offsetX);
-        let my = parseInt(e.clientY - offsetY);
-
-        // calculate the distance the mouse has moved since the last mousemove
-        let dx = mx - startX;
-        let dy = my - startY;
-
-        for (let circle of circles) {
-            if (circle.dragging && !circle.locked) {
-                let newX = circle.x + dx;
-                let newY = circle.y + dy;
-                gfx.redrawImage(circle.img, newX, newY, circle.width, circle.height, circles);
-            }
-        }
-    }
-}
-
-const onPageLoad = async () => {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-
-    let data = await parser.dataFetch("../imageSets.json");
-
-    for (let set of data.sets) {
-        let bgInfo = await gfx.drawBg(set.background, 0.35);
-
-        let icons = set.icons;
-        for (let i = 0; i < icons.length; i++) {
-            circles.push(await gfx.drawImage(icons[i].img, bgInfo.x, bgInfo.y + i * areaSize, 0.05));
-            circles[i].stick = icons[i].stick;
-            circles[i].locked = false;
-        }
-    }
-    console.log(circles);
-
+function preload() {
+    this.load.image('bg'        , '../images/background.jpg');
+    this.load.image('circle'    , '../images/circle.png');
+    this.load.image('poster1'   , '../images/poster.png');
     gfx.toggleLoadingScreen();
+}
+let context;
 
-    canvas.onmousedown = mouseDown;
-    canvas.onmousemove = mouseMove;
-    canvas.onmouseup = mouseUp;
+function create() {
+    context = this;
+   // let bg = this.add.image(width / 2, height / 2, 'bg').setOrigin(0.5);
+   createPosterLayer1();
+
+    let circle = this.add.image(0, 0, "circle").setOrigin(0);
+    circle.scale = 0.05;
+    circle.setInteractive();
+
+    this.input.setDraggable(circle);
+    this.input.dragDistanceThreshold = 5;
+
+    this.input.on('dragstart', function (pointer, gameObject) {
+        gameObject.setTint(0xff0000);
+    });
+
+    this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
+        gameObject.x = dragX;
+        gameObject.y = dragY;
+    });
+
+    this.input.on('dragend', function (pointer, gameObject) {
+        gameObject.clearTint();
+    });
 }
 
-onPageLoad();
+function createPosterLayer1() {
+    let poster = context.add.image(width/2, height/2, 'poster1').setOrigin(0.5);
+}
