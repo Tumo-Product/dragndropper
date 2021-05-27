e806c7558bbde350481f1714d071780356d8418c
HaykAvagyan1 <hayk.avagyan@tumo.org>
2021-05-27 18:56:40 +0400

    Drag and drop working on all resolutions
    You specify drag and drop locations for each icon, it's image, the background in a json file.

--- scripts/flow.js
+++ scripts/flow.js
@@ -1,59 +1,35 @@
-const width = 1080;
-const height = 1080;
-
-let config = {
-    type: Phaser.AUTO,
-    backgroundColor: '#2dab2d',
-    parent: 'phaser-example',
-    scale: {
-        mode: Phaser.Scale.FIT,
-        _parent: 'phaser-example',
-        width: width,
-        height: height
-    },
-    scene: {
-        preload: preload,
-        create: create
-    },
-    transparent : true
-};
-
-let game = new Phaser.Game(config);
-
-function preload() {
-    this.load.image('bg'        , '../images/background.jpg');
-    this.load.image('circle'    , '../images/circle.png');
-    this.load.image('poster1'   , '../images/poster.png');
-    gfx.toggleLoadingScreen();
-}
+let bgPath = "images/background.jpg";
+let icons = [];
 let context;
 
-function create() {
-    context = this;
-   // let bg = this.add.image(width / 2, height / 2, 'bg').setOrigin(0.5);
-   createPosterLayer1();
+const onPageLoad = async () => {
+    let data = await parser.dataFetch("../imageSets.json");
+    let sets = data.sets;
 
-    let circle = this.add.image(0, 0, "circle").setOrigin(0);
-    circle.scale = 0.05;
-    circle.setInteractive();
+    for (let set of sets) {
+        bgPath = set.background;
 
-    this.input.setDraggable(circle);
-    this.input.dragDistanceThreshold = 5;
+        for (let icon of set.icons) {
+            icons.push({ name: icon.name, img: icon.img, stick: { x: icon.stick.x, y: icon.stick.y } });
+        }
+    }
 
-    this.input.on('dragstart', function (pointer, gameObject) {
-        gameObject.setTint(0xff0000);
-    });
-
-    this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
-        gameObject.x = dragX;
-        gameObject.y = dragY;
-    });
-
-    this.input.on('dragend', function (pointer, gameObject) {
-        gameObject.clearTint();
-    });
+    let config = {
+        type: Phaser.AUTO,
+        backgroundColor: '#2dab2d',
+        parent: 'phaser',
+        scale: {
+            mode: Phaser.Scale.FIT,
+            _parent: 'phaser',
+            width: width,
+            height: height
+        },
+        scene: MainScene,
+        transparent: true
+    };
+    
+    let game = new Phaser.Game(config);
 }
 
-function createPosterLayer1() {
-    let poster = context.add.image(width/2, height/2, 'poster1').setOrigin(0.5);
-}
+$(onPageLoad());
+
